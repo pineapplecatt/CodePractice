46689991ecdbf08d0df9fe3ccc76c922c3be1b61
pineapplecatt <mayijing828@gmail.com>
2023-05-26 23:13:49 +0800

    2023.5.26

---
+++ smart_ptr.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <memory>
+
+using namespace std;
+
+int main() {
+    class A {
+       public:
+        void do_something() {}
+    };
+
+    class TestUnique{
+        private:
+         std::unique_ptr<A> a_ = make_unique<A>();
+         public:
+         void process1(){
+             a_->do_something();
+         }
+         void process2(){
+             a_->do_something();
+         }
+         ~TestUnique(){
+            //无需再手动删除a_
+         }
+    };
+
+    /*std::unique_ptr<A> a1 = make_unique<A>();
+    // std::unique_ptr<A> a1(new A());         // 离开a1作用域的时候自动释放内存
+    // std::unique_ptr<A> a2 = a1;             // 编译报错，不允许复制，move-only
+    std::unique_ptr<A> a3 = std::move(a1);  // 可以转移所有权，所有权转移后a1不再拥有任何指针
+
+    // std::unique_ptr<A> a1(new A());
+    A* origin_a = a1.get();  // 尽量不要暴露原生指针
+    if (a1) {
+        // a1 拥有指针
+    }
+
+    std::unique_ptr<A> a2(a1.release());  // 常见用法，转移拥有权
+    a2.reset(new A());                    // 释放并销毁原有对象，持有一个新对象
+    a2.reset();                           // 释放并销毁原有对象，等同于下面的写法
+    a2 = nullptr;                         // 释放并销毁原有对象
+    */
+    /*std::shared_ptr<A> a1(new A());
+    std::shared_ptr<A> a2 = a1;  // 编译正常，允许所有权的共享
+
+    A* origin_a = a1.get();  // 尽量不要暴露原生指针
+
+    if (a1) {
+        // a1 拥有指针
+    }
+
+    if (a1.unique()) {
+        // 如果返回true，引用计数为1
+    }
+
+    long a1_use_count = a1.use_count();  // 引用计数数量*/
+
+    /*std::shared_ptr<A> a1(new A());
+    std::weak_ptr<A> weak_a1 = a1;  // 不增加引用计数
+
+    if (weak_a1.expired()) {
+        // 如果为true，weak_a1对应的原生指针已经被释放了
+    }
+
+    long a1_use_count = weak_a1.use_count();  // 引用计数数量
+
+    if (std::shared_ptr<A> shared_a = weak_a1.lock()) {
+        // 此时可以通过shared_a进行原生指针的方法调用
+    }
+
+    weak_a1.reset();  // 将weak_a1置空*/
+}
